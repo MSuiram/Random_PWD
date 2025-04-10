5d373c411981df1bd3b5e81d843e726f0ae153b2
MSuiram <marius.valard@gmail.com>
2025-04-10 11:14:35 +0200

    Add displays pwd on the sreen

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import random as rd
 import pygame as pg
 import pygame_gui
-from pygame_gui.elements import UILabel,UIButton
+from pygame_gui.elements import UILabel,UIButton,UITextEntryLine
 import sys
-
-def creat_pwd(n_letter, n_number, n_cs):
-    number = [n_letter, n_number, n_cs]
-    cara = ["abcdefgijklmnopqrstuvwxyz","0123456789","*-/+%$#_"]
-    index = 0
-    c = []
-    for i in cara:
-        for n in range(number[index]):
-            c.append(rd.choice(list(i)))
-        index += 0
-    rd.shuffle(c)
-    return "".join(c)
 
 class Choice_number:
     def __init__(self, place : list, UImanager):
@@ -45,7 +33,20 @@ class App:
         self.screen = pg.display.set_mode(self.size)
         self.UImanager = pygame_gui.UIManager(self.size)
         self.UIChoice_number = [Choice_number([800,50],self.UImanager),Choice_number([800,200],self.UImanager),Choice_number([800,350],self.UImanager)]
+        self.UIButton = UIButton(relative_rect = pg.Rect(800,500,100,50), text = "Générer", manager= self.UImanager)
+        self.UILabel = UITextEntryLine(relative_rect = pg.Rect(150,350,400,100), initial_text= "Clicer sur Générer", manager= self.UImanager)
 
+    def creat_pwd(self, n_letter, n_number, n_cs):
+        number = [n_letter, n_number, n_cs]
+        cara = ["abcdefgijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ","0123456789","*-/+%$#_"]
+        index = 0
+        c = []
+        for i in cara:
+            for n in range(number[index]):
+                c.append(rd.choice(list(i)))
+            index += 1
+        rd.shuffle(c)
+        return "".join(c)
 
     def run(self):
         clock = pg.time.Clock()
@@ -65,6 +66,9 @@ class App:
                                 self.UIChoice_number[i].Up()
                             if event.ui_element is self.UIChoice_number[i].UI[2]:
                                 self.UIChoice_number[i].Down()
+                        
+                        if event.ui_element is self.UIButton:
+                            self.UILabel.set_text(str(self.creat_pwd(self.UIChoice_number[0].n,self.UIChoice_number[1].n,self.UIChoice_number[2].n)))
 
             self.UImanager.update(time_delta/1000)
             self.UImanager.draw_ui(self.screen)
